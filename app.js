861684bb118887e64abb4325fae8b9480095bb81
jaypersanchez <jaypersanchez@gmail.com>
2023-11-15 20:04:23 +0800

    saving login and new account end points

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const connectToDatabase = require('./db')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 const User = require('./models/user'); 
 
@@ -18,7 +21,7 @@ app.use(cors());
 connectToDatabase();
 
 // Replace this with your actual secret key. It should be kept secret.
-const secretKey = 'yourSecretKey';
+const secretKey = process.env.SECRET_KEY
 // Sample user data (usually, you'd retrieve this from a database)
 const users = [];
 
@@ -53,99 +56,67 @@ app.get('/get-settings', (req, res) => {
     // You can return data or perform other actions here
 });
 
-app.get('/protected', (req, res) => {
-    const token = req.header('Authorization');
-  
-    if (!token) {
-      return res.status(401).json({ message: 'Authorization token is missing' });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, secretKey);
-      const userId = decoded.userId;
-  
-      // You can now use the user ID to fetch user-specific data from the database
-      // For this example, we'll just return a message.
-      res.json({ message: `Protected data for user with ID ${userId}` });
-    } catch (err) {
-      res.status(401).json({ message: 'Invalid token' });
-    }
-});
 
-// Post route methods
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
   
-    // Check if the username already exists
-    if (users.some((user) => user.username === username)) {
-      return res.status(400).json({ message: 'Username already taken' });
+    // Check if the user with the given email exists in the database
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "failed", error: 'User not found' });
     }
-  
-    // Hash the password
-    const hashedPassword = bcrypt.hashSync(password, 10); // Hash the password with a salt round of 10
-  
-    const user = {
-      id: users.length + 1,
-      username,
-      password: hashedPassword,
-    };
-  
-    users.push(user);
-  
-    res.status(201).json({ message: 'User registered successfully' });
-  });
-  
-  app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    // Check if the username exists
-    const user = users.find((u) => u.username === username);
-  
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-  
+        
     // Verify the password
-    if (bcrypt.compareSync(password, user.password)) {
-      // Passwords match, generate a JWT
-      const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
-      res.json({ token });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
-    }
+    bcrypt.compare(req.body.password, existingUser.password, (err, data) => {
+        //if error than throw error
+        if (err) throw err
+
+        //if both match than you can do anything
+        if (data) {
+            // Passwords match, generate a JWT
+            const token = jwt.sign({ userId: existingUser.id }, secretKey, { expiresIn: '24h' });
+            return res.status(200).json({ data:data, token: token })
+        } else {
+            return res.status(401).json({ data: data, msg: "Invalid credencial" })
+        }
+    })
   });
 
-app.post('/post-users', (req, res) => {
-    // Handle the GET request for the "users" route
-    // You can return data or perform other actions here
-});
  
 //this function will also be used to register new accounts and update profiles
-app.post('/post-profile', async (req, res) => {
+app.post('/new-account', async (req, res) => {
 
   try {
       // Retrieve data from the request body
       const { email, firstName, lastName, mobile, password } = req.body;
+      
+      // Check if the user with the given email exists in the database
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(404).json({ message: "failed", error: 'Email already exist' });
+      }
 
       // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
+      //const hashedPassword = await bcrypt.hashSync(password, 10);
       // Create a new user instance
       const newUser = new User({
         email,
         firstName,
         lastName,
         mobile,
-        password: hashedPassword, // Save the hashed password
+        password: password, // Save the hashed password
       });
 
       // Save the user to the database
       await newUser.save();
-
-      // Perform actions with the received data
-      console.log('Received data:', { email, firstName, lastName, mobile, hashedPassword });
       
-
-      return res.status(200).json({ message: 'success', email: email });
+      // Perform actions with the received data
+      console.log('Received data:', { email, firstName, lastName, mobile, password });
+      const token = jwt.sign({ userId: newUser.id }, secretKey, { expiresIn: '24h' });
+            
+      return res.status(200).json({ message: 'success', email: email, token: token });
   }
   catch(error) {
     console.error('Error:', error);
--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1/helpgenie');
+    const DB_URI = process.env.DB_URI
+    await mongoose.connect(DB_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
